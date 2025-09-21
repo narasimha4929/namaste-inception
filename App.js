c4790ba7c4d1e7fd5b29573d794fc57a34bafa27
narasimha4929 <73059863+narasimha4929@users.noreply.github.com>
2025-09-21 16:13:30 +0530

    parcel added

--- App.js
+++ App.js
@@ -1,45 +1,33 @@
-/**
- * <div id ="parent">
- *     <div id ="child">
- *        <h1>
- *        </h1>
- *        <h2>
- *        </h2>
- *     <div/>
- * 
- * </div>
- * 
- * 
- * ReactElement OBJECT -> HTML Browser understands 
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    [
-      React.createElement("h1", {}, "THis is inside the div->div->h1"),
-      React.createElement("h2", {}, "THis is inside the div->div->h2"),
-    ],
-    [
-      React.createElement("h1", {}, "THis is inside the div->div->h1"),
-      React.createElement("h2", {}, "THis is inside the div->div->h2"),
-    ],
+    React.createElement(
+      "h1",
+      { key: "h1-1" },
+      "This is inside the div->div->h1"
+    ),
+    React.createElement(
+      "h2",
+      { key: "h2-1" },
+      "This is inside the div->div->h2"
+    ),
+    React.createElement(
+      "h1",
+      { key: "h1-2" },
+      "This is inside the div->div->h1"
+    ),
+    React.createElement(
+      "h2",
+      { key: "h2-2" },
+      "This is inside the div->div->h2"
+    ),
   ])
 );
 
-//order of sequence is important
-
-
-
-const heading = React.createElement(
-  "h1",//type
-  {id:"heading",xyz:"abc"},//props
-  "Hello World from React Inside the DOM Children"//children
-);
-// console.log(heading);
-// console.log(heading.props);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log(parent);
-root.render(parent);// take this and create a link for the root
+root.render(parent);
---
+++ Readme.md
@@ -0,0 +1,18 @@
+# parcel
+
+- Dev build
+- Local server
+- HMR = hot module replacement
+- File watching algorithm - writen in c++
+- caching - Faster Builds
+- Image - optimization
+- Minification
+- Bundling
+- Compressing
+- consistent Hashing
+- code splitting
+- Differential Bundling - supports in old browsers
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree shaking - remove unused code 
